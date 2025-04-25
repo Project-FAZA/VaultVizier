58e388727acb4e0e4531ea8d08d8da006eeeb73c
FadTheChad <fmzfads@gmail.com>
2025-04-26 01:42:54 +0500

    temp made dark mode transparent

--- src/classes/GlobalVars.hpp
+++ src/classes/GlobalVars.hpp
@@ -7,8 +7,8 @@
 class GlobalVar
 {
 public:
-    static inline ftxui::Color getColor() { return darkMode ? ftxui::Color::RGB(161, 235, 255) : ftxui::Color::RGB(0, 0, 0); }
-    static inline ftxui::Color getBg() { return darkMode ? ftxui::Color::RGB(10, 10, 29) : ftxui::Color::RGB(230, 230, 230); }
+    static inline ftxui::Color getColor() { return darkMode ? ftxui::Color::RGBA(161, 235, 255, 0) : ftxui::Color::RGB(0, 0, 0); }
+    static inline ftxui::Color getBg() { return darkMode ? ftxui::Color::RGBA(10, 10, 29, 0) : ftxui::Color::RGB(230, 230, 230); }
 
     static inline bool darkMode = true;
 };
