0fc29649c9f7454d9791f4dd8d78ed2192e282e7
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2025-04-26 21:40:20 +0500

    Update Patient.hpp
    added phone number, weight, height, and marital status in Patient class
    edit by Zain Bhai

--- src/classes/Patient.hpp
+++ src/classes/Patient.hpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cctype>
 
 using namespace std;
 
@@ -16,7 +17,6 @@ struct Date
     int y;
 };
 
-bool isValidDate(const Date &date);
 
 struct Address
 {
@@ -27,6 +27,12 @@ struct Address
     string country;
 };
 
+//validation func
+bool isValidDate(const Date &date);
+string validateField(const string& field, int maxLength, const string& fieldName);
+bool isAllDigits(const string& s);
+bool validatePhoneNumber(const string& phone);
+
 class Patient
 {
 private:
@@ -34,23 +40,37 @@ private:
     string lastName;
     Date dob;
     Address address;
+    bool gender; // F = female , T = male
+    string phoneNumber; //+92-301-23456789
+    float weight; //kg
+    float height; //ft
+    int maritalStatus; //{-1 = widowed, 0 = single, 1 = married}
 
 public:
     Patient(const string &fname, const string &lname, const Date &date, const Address &addr);
 
     // Setters
-    void
-    setFirstName(const string &fname);
+    void setFirstName(const string &fname);
     void setLastName(const string &lname);
     void setDOB(const Date &date);
     void setAddress(const Address &addr);
+    void setGender(const bool &sex);
+    void setPhoneNumber(const string &num);
+    void setWeight(const float &w);
+    void setHeight(const float &h);  
+    void setMaritalStatus(const int &married);
 
     // Getters
     string getFirstName() const;
     string getLastName() const;
     Date getDOB() const;
     Address getAddress() const;
-
+    bool getGender() const;
+    string getPhoneNumber() const;
+    float getHeight() const ;  
+    float getWeight() const ;
+    int getMaritalStatus() const ;
+    
     // adds/updates to actual database;
     void save();
     static vector<Patient> fetchAll();
