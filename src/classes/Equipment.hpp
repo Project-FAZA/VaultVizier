dd6b5f7ddc9be76f9678f4067142f3adc15c98ae
FadTheChad <fmzfads@gmail.com>
2025-05-03 01:30:18 +0500

    temp

---
+++ src/classes/Equipment.hpp
@@ -0,0 +1,16 @@
+#ifndef EQ_HPP
+#define EQ_HPP
+
+#include <iostream>
+using namespace std;
+
+class Equipment
+{
+private:
+    string code;
+    bool requiresMedicare;
+    double price;
+    int quantity;
+};
+
+#endif
