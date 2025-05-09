58b551fcf9ac762bb33ae9f0fa115f0988d389af
Zain ul abideen Ahmad <146072161+Zedstorm9246@users.noreply.github.com>
2025-05-09 22:05:42 +0500

    Update Equipment.hpp
    getter and setters

--- src/classes/Equipment.hpp
+++ src/classes/Equipment.hpp
@@ -4,13 +4,31 @@
 #include <iostream>
 using namespace std;
 
+class Patient;
+
 class Equipment
 {
+
 private:
     string code;
-    bool requiresMedicare;
+    bool requiresMedicare; //if false, then it requires Medicaid
     double price;
-    int quantity;
+
+public: 
+
+    Equipment(const string &c, const bool &reqMed, const double &p);
+    //setters
+    void setCode(const string &c);
+    void setRequiresMedicare(const bool &medi);
+    void setPrice(const double &p);
+
+    //getters
+    string getCode();
+    bool getRequiresMedicare();
+    double getPrice();
+
+    double calculateActualPrice(Patient &pt);
+
 };
 
 #endif
